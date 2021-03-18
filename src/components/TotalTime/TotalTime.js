ec5ff5b50c60f971959d8c8ae0b61fe0f8ad6989
Mike Sterling <mbsterlingconsulting@gmail.com>
2021-03-18 12:37:11 -0500

    TotalTime control 1st iteration

--- src/components/TotalTime/TotalTime.js
+++ src/components/TotalTime/TotalTime.js
@@ -1,14 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+//https://momentjs.com/
+import moment from 'moment';
 
 const TotalTime = (props) => {
 
-    useEffect(() => {
-        console.log(props.timein);
+    const[totalTime, setTotalTime] = useState("00:00:00");
+
+    useEffect(() => {        
+        let now = Date().toLocaleString();
+        let timeIn = new Date(props.timein);
+        console.log(now + '' + timeIn);
+
+        now = moment(now);
+        timeIn = moment(timeIn);
+
+        var differceBetweenTheDates = moment(now).diff(moment(timeIn), 'minutes');
+        var d = Math.floor(differceBetweenTheDates / 1440);
+        var remainingMinutes = differceBetweenTheDates % 1440;
+        var h = Math.floor(remainingMinutes / 60);
+        var m = remainingMinutes % 60;
+        
+        console.log("00:" + h.toString() + ":" + (m<10?"0":"") + m.toString());
+        setTotalTime(d.toString() + ":" + h.toString() + ":" + m.toString());
+        
     }, []);
 
     return (
         <div>
-            <p>00:40:00</p>
+            <p>{totalTime}</p>
         </div>
     );
 }
